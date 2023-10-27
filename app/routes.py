8ccccfe88937ec6fab62a56f009fa1e6bbf9155e
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-10-27 20:31:58 +0200

    Complete frontend styling

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,16 @@
 from app import app
 from flask import render_template, request, make_response, jsonify
 
-# Define a route to retrieve HTML page
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+# Define a route to retrieve event page
+@app.route('/event/<event_id>')
+def event_page(event_id):
+    # Render the HTML template by passing the event_id to it
+    html_content = render_template('event.html', event_id=event_id)
+
+    # Create a response
+    response = make_response(html_content)
+
+    # Set the event ID in the custom response header
+    response.headers['X-Event-ID'] = event_id
+
+    return response
