dcb0cde9624c6fc0b50f2ae56fc3d07f25feb521
Ahmedsaed <ahmedsaed2652003@gmail.com>
2023-10-27 20:32:21 +0200

    configure the api to work with the app

--- api/event.py
+++ api/event.py
@@ -1,26 +1,6 @@
 from app import app
 from flask import render_template, request, make_response, jsonify
 
-# Define a route to retrieve HTML page
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
-
-# Define a route to retrieve event page
-@app.route('/event/<event_id>')
-def event_page(event_id):
-    # Render the HTML template by passing the event_id to it
-    html_content = render_template('event.html', event_id=event_id)
-
-    # Create a response
-    response = make_response(html_content)
-
-    # Set the event ID in the custom response header
-    response.headers['X-Event-ID'] = event_id
-
-    return response
-
 
 # Define directory where event details are stored
 event_file_storage = '/test/'
@@ -36,7 +16,7 @@ user_file_storage = '/test2/'
 
 # Define a route to retrieve user details
 @app.route('/api/user/<user_details>')
-def get_user_details(user_id):
+def get_user_details(user_details):
     return jsonify(user_file_storage)
 
 
