beb5e7f36a17a8540aac871539e3e7305b2a893a
adhamessammetwally@gmail.com <adhamessammetwally@gmail.com>
2023-11-09 18:49:18 +0200

    made code cleaner

--- app/static/scripts/dashboard.js
+++ app/static/scripts/dashboard.js
@@ -2,12 +2,7 @@ const expandBtns = document.querySelectorAll('.expand-btn');
 
 expandBtns.forEach(btn => {
 	btn.addEventListener('click', () => {
-		btn
-		.parentNode
-		.parentNode
-		.querySelectorAll('.event-details')[0]
-		.classList.toggle('expand');
-
+		btn.parentNode.parentNode.querySelectorAll('.event-details')[0].classList.toggle('expand');
 		btn.classList.toggle('expanded');
 	});
 });
